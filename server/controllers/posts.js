21fc46e47d1afe3517ccf1fc46b6e376307441ba
Leo Babakhanian <babakhanianleo@gmail.com>
2022-03-16 02:13:49 -0700

    poll prototype

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -57,29 +57,4 @@ export const deletePost = async (req, res) => {
   res.json({ message: "Post deleted successfully." });
 };
 
-export const likePost = async (req, res) => {
-  const { id } = req.params;
-
-  if (!req.userId) {
-    return res.json({ message: "Not authenticated" });
-  }
-
-  if (!mongoose.Types.ObjectId.isValid(id))
-    return res.status(404).send(`No post with id: ${id}`);
-
-  const post = await PostMessage.findById(id);
-
-  const index = post.likes.findIndex((id) => id === String(req.userId));
-
-  if (index === -1) {
-    post.likes.push(req.userId);
-  } else {
-    post.likes = post.likes.filter((id) => id !== String(req.userId));
-  }
-  const updatedPost = await PostMessage.findByIdAndUpdate(id, post, {
-    new: true,
-  });
-  res.status(200).json(updatedPost);
-};
-
 export default router;
