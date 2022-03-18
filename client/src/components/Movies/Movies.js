362a80e0999a483981e4be962753bbc838f989ed
Leo Babakhanian <babakhanianleo@gmail.com>
2022-03-17 21:11:36 -0700

    tweaked movie search

--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./Movies.css";
 import SearchIcon from "@mui/icons-material/Search";
-import { Button } from "@mui/material";
+import { Button, Grow } from "@mui/material";
 import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=eed0702d";
@@ -18,34 +18,35 @@ const Movies = () => {
   };
 
   useEffect(() => {
-    searchMovies("Spiderman");
+    searchMovies("Batman");
   }, []);
 
   return (
-    <div className="app">
-      <div className="search">
-        <input
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for movies"
-        />
-        <Button onClick={() => searchMovies(searchTerm)}>
-          <SearchIcon />
-        </Button>
+    <Grow in>
+      <div className="app">
+        <div className="search">
+          <input
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search for movies"
+          />
+          <Button onClick={() => searchMovies(searchTerm)}>
+            <SearchIcon />
+          </Button>
+        </div>
+        {movies?.length > 0 ? (
+          <div className="movie-container">
+            {movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))}
+          </div>
+        ) : (
+          <div className="empty">
+            <h2>No movies found</h2>
+          </div>
+        )}
       </div>
-
-      {movies?.length > 0 ? (
-        <div className="movie-container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
-        </div>
-      ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
-      )}
-    </div>
+    </Grow>
   );
 };
 
