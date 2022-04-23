1d20d819663b447821b48de7afc4a46ec396a332
Leo Babakhanian <babakhanianleo@gmail.com>
2022-04-23 02:40:35 -0700

    removed case sensitivity for emails

--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -1,53 +1,53 @@
 import React, { useState, useEffect } from "react";
 import "./Movies.css";
 import SearchIcon from "@mui/icons-material/Search";
-import { Button, Grow } from "@mui/material";
+import { IconButton, Grow } from "@mui/material";
 import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=eed0702d";
 
 const Movies = () => {
-  const [searchTerm, setSearchTerm] = useState("");
-  const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState("");
+	const [movies, setMovies] = useState([]);
 
-  const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
 
-    setMovies(data.Search);
-  };
+		setMovies(data.Search);
+	};
 
-  useEffect(() => {
-    searchMovies("Batman");
-  }, []);
+	useEffect(() => {
+		searchMovies("Batman");
+	}, []);
 
-  return (
-    <Grow in>
-      <div className="app">
-        <div className="search">
-          <input
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            placeholder="Search for movies"
-          />
-          <Button onClick={() => searchMovies(searchTerm)}>
-            <SearchIcon />
-          </Button>
-        </div>
-        {movies?.length > 0 ? (
-          <div className="movie-container">
-            {movies.map((movie) => (
-              <MovieCard movie={movie} />
-            ))}
-          </div>
-        ) : (
-          <div className="empty">
-            <h2>No movies found</h2>
-          </div>
-        )}
-      </div>
-    </Grow>
-  );
+	return (
+		<Grow in>
+			<div className="app">
+				<div className="search">
+					<input
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+						placeholder="Search for movies"
+					/>
+					<IconButton onClick={() => searchMovies(searchTerm)}>
+						<SearchIcon style={{ color: "#fff", fontSize: 30 }} />
+					</IconButton>
+				</div>
+				{movies?.length > 0 ? (
+					<div className="movie-container">
+						{movies.map((movie) => (
+							<MovieCard movie={movie} />
+						))}
+					</div>
+				) : (
+					<div className="empty">
+						<h2>No movies found</h2>
+					</div>
+				)}
+			</div>
+		</Grow>
+	);
 };
 
 export default Movies;
